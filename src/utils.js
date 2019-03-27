f2200c020b76e58901b6fc726b36c01ceda64dba
David Liu <msglight4874@gmail.com>
2019-03-28 00:38:43 +0800

    add validateEmail & validatePhone to utils

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+export function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(email).toLowerCase());
+}
+
+export function validatePhone(phone) {
+  var re = /^\d{2}-\d{7}$/;
+  return re.test(String(phone).toLowerCase());
+}
