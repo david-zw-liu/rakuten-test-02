f0da2718dc2fa23785bc9366e635d9f857b84213
David Liu <msglight4874@gmail.com>
2019-03-27 22:19:24 +0800

    add transform option to column options

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function TableBody(props) {
   const { data, columns } = props;
   const trs = data.map((item, idx) => {
-    const tds = [
-      <td key="no">{ idx + 1 }</td>,
-      ...columns.map(({ key }) => <td key={key}>{ item[key] }</td>)
-    ]
+    const tds = [<td key="no">{ idx + 1 }</td>]
+    tds.push(columns.map(({ key, transform }) =>{
+      return <td key={key}>{ transform ? transform(item) : item[key] }</td> 
+    }));
     const trKey = Object.values(item).toString();
 
     return (
